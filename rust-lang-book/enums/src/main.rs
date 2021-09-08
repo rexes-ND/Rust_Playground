c955116ce2306048c9e57ce5929918e94901f46c
Erkhes <erkhes.nyamsaikhan@gmail.com>
2021-09-08 20:07:19 +0900

    Finished Chapter 6

---
+++ rust-lang-book/enums/src/main.rs
@@ -0,0 +1,115 @@
+fn main() {
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
+
+    // let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    // let loopback = IpAddr::V6(String::from("::1"));
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    // let some_number = Some(5);
+    // let some_string = Some("a string");
+
+    // let absent_number: Option<i32> = None;
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
+    let some_u8_value = Some(0u8);
+    // match some_u8_value {
+    //     Some(3) => println!("three"),
+    //     _ => (),
+    // }
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+}
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        // Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+
+// fn route(ip_kind: IpAddrKind) {}
+
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+
+// struct Ipv4Addr {
+//     // --snip--
+// }
+
+// struct Ipv6Addr {
+//     // --snip--
+// }
+
+// enum IpAddr {
+//     V4(Ipv4Addr),
+//     V6(Ipv6Addr),
+// }
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
